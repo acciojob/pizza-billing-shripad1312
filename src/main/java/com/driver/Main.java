087b778811c79e9bda4488f3a64a928da85af7e0
despo <shhripadb.20@gmail.com>
2023-09-16 01:57:52 +0530

    5thc

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,19 +4,19 @@ public class Main {
   public static void main(String[] args) {
 
     DeluxePizza dp = new DeluxePizza(true);
-    //System.out.println(dp.getPrice());
+    System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
 
     dp.addTakeaway();
-    //System.out.println()
-    dp.getBill();
+    System.out.println(dp.getBill();)
+
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
-    //System.out.println()
-    p.getBill();
+    System.out.println(p.getBill());
+
   }
 }
