d358e79222994674a9423458b54b0b8bcb472d44
despo <shhripadb.20@gmail.com>
2023-09-16 01:50:59 +0530

    4thc

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,8 +4,8 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
-       // super.getPrice();
+
+      // super.getPrice();
         super.addExtraCheese();
         super.addExtraToppings();
     }
